43dd248e52b01143858801bb2549ac953c4612d0
Seah Bei Ying <beiyingseah@gmail.com>
2022-09-13 12:01:45 +0300

    Fixed bug for global state

--- helpers/gameboard.js
+++ helpers/gameboard.js
@@ -1,9 +1,3 @@
-// Main app
-// Rest of the page elements controlled directly by user
-const newGameBtn = document.getElementsByClassName('new-game-btn')[0];
-
-newGameBtn.onclick = () => gameboardDisplayController.newGame();
-
 // Modules
 export const gameboard = (() => {
     const cellElements = document.querySelectorAll('[data-cell]');
@@ -34,106 +28,119 @@ export const gameboardDisplayController = (() => {
         [2,4,6]
     ];
 
-    let circleTurn;
-    
-    const swapTurns = () => {
-        circleTurn = !circleTurn;
-    }
-
-    // Results
-    const checkWin = (currentClass) => {
-    return WINNING_COMBINATIONS.some(combination => {
-        return combination.every(index => {
-            return cellElements[index].classList.contains(currentClass) 
-            }) 
-        })
-    };
-    
-    const checkDraw = () => {
-    return [...cellElements].every(cell => {
-        return cell.classList.contains(X_CLASS) || cell.classList.contains(CIRCLE_CLASS);
-        });
-    };
-    
-    const endGame = (draw) => {
-    if (draw) {
-        modalDisplayController.resultMessage = `It's a draw!`
-        modalDisplayController.openModal();
-
-    } else {
-        modalDisplayController.resultMessage = `${circleTurn ? 'O' : 'X'} wins this round!`
-        modalDisplayController.openModal();
-        }      
-    };
+    // FIX THE GLOBAL STATE
+    const gameboardState = (() => {
+        let circleTurn;
+        return {circleTurn};
+    })(); 
 
     // Gameboard Display
     const setBoardHoverClass = (circleTurn) => {
-        board.classList.remove(X_CLASS);
-        board.classList.remove(CIRCLE_CLASS);
-        if (circleTurn) {
-            board.classList.add(CIRCLE_CLASS);
+        console.log('SET BOARD HOVER CLASS');
+        console.log(`circle turn is ${gameboardState.circleTurn}`);
+        console.log(`gameboard.board.classList is ${gameboard.board.classList}`);
+        gameboard.board.classList.remove(X_CLASS);
+        gameboard.board.classList.remove(CIRCLE_CLASS);
+        if (gameboardState.circleTurn) {
+            console.log(`gameboardState.circleTurn = true, add circle_class, ${CIRCLE_CLASS}`);
+            gameboard.board.classList.add(CIRCLE_CLASS);
         } else {
-            board.classList.add(X_CLASS);
+            console.log(`xTurn = true, add x_class, ${X_CLASS}`);
+            gameboard.board.classList.add(X_CLASS);
         }
-        console.log(`Board hover class: ${board.classList}`);
+        console.log(`Board hover class: ${gameboard.board.classList}`);
     };  
 
     const placeMark = (cell, currentClass) => {
         cell.classList.add(currentClass);
     }
+
+    const swapTurns = (circleTurn) => {
+        console.log('SWAP TURNS');
+        gameboardState.circleTurn = !gameboardState.circleTurn;
+        console.log(`circle turn is ${gameboardState.circleTurn}`);
+}
+
+    // Results
+    const checkWin = (currentClass) => {
+        return WINNING_COMBINATIONS.some(combination => {
+            return combination.every(index => {
+                return gameboard.cellElements[index].classList.contains(currentClass);
+                }) 
+            })
+        };
+        
+        const checkDraw = () => {
+        return [...gameboard.cellElements].every(cell => {
+            return cell.classList.contains(X_CLASS) || cell.classList.contains(CIRCLE_CLASS);
+            });
+        };
+        
+        const endGame = (draw, p1Name, p2Name) => { //with 'draw' being a boolean, true or false
+        if (draw) {
+            modalDisplayController.resultMessage = `It's a draw!`
+            modalDisplayController.openModal();
     
+        } else {
+            modalDisplayController.resultMessage = `${gameboardState.circleTurn ? `${p2Name}` : `${p1Name}`} wins this round!`
+            modalDisplayController.openModal();
+            }      
+        };
+        
     // So much logic that goes into a click! :o
     const handleClick = (e) => { 
         const cell = e.target;
-        console.log('clicked');
-        const currentClass = circleTurn ? CIRCLE_CLASS : X_CLASS;
+        console.log(`CELLL CLICKED: ${cell}`);
+        console.log(`circle turn: ${gameboardState.circleTurn}`);
+        const currentClass = gameboardState.circleTurn ? CIRCLE_CLASS : X_CLASS;
+        console.log(`currentClass: ${currentClass}`);
         placeMark(cell, currentClass);
         // Check for Win
         if (checkWin(currentClass)) {
-            console.log('winner');
-            endGame(false);
+            endGame(false, player1.name, player2.name);
+            console.log('A PLAYER WINS!');
             if (currentClass === player1.symbol) {
                 player1.addScore;
-                updateScoreboard();
+                console.log(player1);
             } else {
                 player2.addScore;
-                updateScoreboard();
+                console.log(player2);
             }
+            updateScoreboard(player1.score, player2.score);
         }
         else if (checkDraw()) {
-            console.log('draw')
+            console.log('DRAW!')
             endGame(true);
     
         } else {
-            console.log('next turn');
-            swapTurns(); 
+            swapTurns(gameboardState.circleTurn); 
             setBoardHoverClass();
         }
     } 
 
     const clearGameboard = () => {
-        board.classList.remove(X_CLASS);
-        board.classList.remove(CIRCLE_CLASS);
-        cellElements.forEach(cell => {
+        gameboard.board.classList.remove(X_CLASS);
+        gameboard.board.classList.remove(CIRCLE_CLASS);
+        gameboard.cellElements.forEach(cell => {
             cell.classList.remove(X_CLASS);
             cell.classList.remove(CIRCLE_CLASS);
         });
     }
     
-    const renderScoreboard = (player1Name, player2Name, player1Score, player2Score) => {
+    const renderScoreboard = (p1Name, p2Name, p1Score, p2Score) => {
         console.log('renderScoreboard');
-        gameboard.p1NameElement.textContent = player1Name + " [X]";
-        gameboard.p2NameElement.textContent = player2Name + " [O]";
-        gameboard.xScoreElement.textContent = player1Score;
-        gameboard.circleScoreElement.textContent = player2Score;
+        gameboard.p1NameElement.textContent = p1Name + " [X]";
+        gameboard.p2NameElement.textContent = p2Name + " [O]";
+        gameboard.xScoreElement.textContent = p1Score;
+        gameboard.circleScoreElement.textContent = p2Score;
     }
     
     //TODO; to pass in player1.score, player2.score from player object instances created in setup.html
-    const updateScoreboard = (player1Score, player2Score) => {
+    const updateScoreboard = (p1Score, p2Score) => {
         // Player 1 is X, player 2 is O
         // Mapping: Player 1 Name <> X symbol, when X wins, player's score increases, scoreboard renders accordingly to reflect the score.
-        gameboard.xScoreElement.textContent = player1Score;
-        gameboard.circleScoreElement.textContent = player2Score;
+        gameboard.xScoreElement.textContent = p1Score;
+        gameboard.circleScoreElement.textContent = p2Score;
     } 
     
     const resetScoreboard = () => {
@@ -143,14 +150,17 @@ export const gameboardDisplayController = (() => {
         gameboard.circleScoreElement.textContent = 0;
     }
 
-    const startGame = () => {
-        playerFirstMove = !circleTurn;
-        circleTurn = playerFirstMove;
-        console.log(`startGame, player first move is: ${playerFirstMove}`);
-        cellElements.forEach(cell => {
+    const startGame = (player1Obj, player2Obj) => {
+        console.log('START GAME');
+        console.log(`circle turn is ${gameboardState.circleTurn}`);
+        let playerFirstMove = !gameboardState.circleTurn;
+        gameboardState.circleTurn = playerFirstMove;
+        console.log(`circle turn is ${gameboardState.circleTurn}`);
+        console.log(`startGame, gameboardState.circleTurn & player first move is: ${playerFirstMove}`);
+        gameboard.cellElements.forEach(cell => {
             cell.addEventListener('click', handleClick, { once: true })
         })
-        setBoardHoverClass();
+        setBoardHoverClass(gameboardState.circleTurn);
     };
     
     const newGame = () => {
@@ -160,6 +170,7 @@ export const gameboardDisplayController = (() => {
         resetScoreboard();
         console.log('Directed to setup.html');
         window.location='index.html';
+        setBoardHoverClass();
     };
 
     return {startGame, newGame, resetScoreboard, renderScoreboard};
--- pages/main.js
+++ pages/main.js
@@ -1,6 +1,8 @@
 import { gameboard, gameboardDisplayController, modalDisplayController} from '../helpers/gameboard.js';
 import Player from '../helpers/players.js';
 
+// Main app
+// Player inputs from setup.html
 const player1 = Player(localStorage.getItem('p1Name'), 'x');
 const player2 = Player(localStorage.getItem('p2Name'), 'o');
 
@@ -8,7 +10,11 @@ console.log(player1);
 console.log(player2);
 
 gameboardDisplayController.renderScoreboard(player1.name, player2.name, player1.score, player2.score);
+gameboardDisplayController.startGame(player1, player2);
 
-startGame()
+// Rest of the page elements controlled directly by user
+const newGameBtn = document.getElementsByClassName('new-game-btn')[0];
+
+newGameBtn.onclick = () => gameboardDisplayController.newGame();
 
 //renderScoreboard(p1NameValue, p2NameValue);
